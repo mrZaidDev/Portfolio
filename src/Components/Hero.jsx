3f66a167104c3ebacd7cc066e23f98a40ebc2aeb
Zaid Khan <zaidcodingtopcoding@gmail.com>
2025-05-29 23:05:03 +0500

    edited some responsiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -23,13 +23,13 @@ const hero = () => {
       <div className="max-w-full h-[75vh] mx-auto flex px-10 items-center justify-center flex-col md:flex-row ">
         {/* Content */}
         <div className="flex flex-col gap-5 text-center mt-4 order-2 
-          xs:w-[40%] sm:w-[50%] md:w-[500px] md:pb-25 pl-10 md:text-start ">
-          <h1 className="text-4xl font-bold md:text-5xl">
+          xs:w-[50%] sm:w-[50%] md:w-[500px] md:pb-25 pl-10 md:text-start ">
+          <h1 className="text-3xl xs:text-3xl sm:text-4xl font-bold md:text-5xl">
             Front-End React Developer{" "}
             <img src={waving_hand} alt="" className="w-[50px] inline" />
           </h1>
           <p className="text-gray-500 font-semibold">
-            Hi, I'm Zaid Noorzai. A passionate Front end React Developer based
+           I'm Zaid Noorzai. A passionate Front end React Developer based
             in Pakistan, Chaman
           </p>
           {/* Icons start */}
@@ -45,7 +45,7 @@ const hero = () => {
             {/* Icons end*/}
         </div>
         {/* Image */}
-        <img className="order-1 w-[250px] h-[50%] rounded-2xl xs:w-[150px] sm:w-[200px] md:w-[250px]" src={zaid_img} alt="" />
+        <img className="order-1 w-[180px] h-[230px] rounded-2xl sm:w-[200px] sm:h-[250px] md:w-[230px] ml-9 md:mb-15" src={zaid_img} alt="" />
       </div>
        {/* Tech Stack */}
         <div className="mt-[30px] flex items-center justify-center gap-5  text-[18px] font-bold h-[12vh]">
