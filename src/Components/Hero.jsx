1ea238846ecac2fe768358663a84c7c67d452ced
Zaid Khan <zaidcodingtopcoding@gmail.com>
2025-05-30 14:33:25 +0500

    modified my hero section for mobile responsiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -20,13 +20,15 @@ const hero = () => {
       </nav>
 
       {/* Main Content */}
-      <div className="max-w-full h-[75vh] mx-auto flex px-10 items-center justify-center flex-col md:flex-row ">
+      <div className="px-10 max-w-full h-[75vh] mx-auto flex items-center justify-center flex-col md:flex-row ">
+          {/* Image */}
+        <img className="w-[200px] h-[230px] rounded-2xl  sm:h-[250px] sm:w-[220px] md:w-[250px] md:h-[280px]  md:mb-15" src={zaid_img} alt="" />
         {/* Content */}
-        <div className="flex flex-col gap-5 text-center mt-4 order-2 
-          xs:w-[50%] sm:w-[50%] md:w-[500px] md:pb-25 pl-10 md:text-start ">
-          <h1 className="text-3xl xs:text-3xl sm:text-4xl font-bold md:text-5xl">
-            Front-End React Developer{" "}
-            <img src={waving_hand} alt="" className="w-[50px] inline" />
+        <div className="flex flex-col gap-5 text-center mt-4
+          w-[300px] sm:w-[400px] md:mt-0 md:w-[500px] md:pb-25 md:ml-10  md:text-start ">
+          <h1 className="text-3xl  sm:text-3xl font-bold md:text-5xl">
+            Front-End React Developer {" "}
+            <img src={waving_hand} alt="" className="w-[35px] inline sm:w-[40px] md:w-[50px]" />
           </h1>
           <p className="text-gray-500 font-semibold">
            I'm Zaid Noorzai. A passionate Front end React Developer based
@@ -44,8 +46,7 @@ const hero = () => {
           </div>
             {/* Icons end*/}
         </div>
-        {/* Image */}
-        <img className="order-1 w-[180px] h-[230px] rounded-2xl sm:w-[200px] sm:h-[250px] md:w-[230px] ml-9 md:mb-15" src={zaid_img} alt="" />
+      
       </div>
        {/* Tech Stack */}
         <div className="mt-[30px] flex items-center justify-center gap-5  text-[18px] font-bold h-[12vh]">
